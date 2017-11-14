89a4a1243139bc7091c204b08ae778e55c9860ad
PowerOfDark <przem2003@o2.pl>
2017-11-14 18:20:58 +0100

    Add Windows Desktop target (DirectX)

---
+++ Rzut.WinDesktop/Program.cs
@@ -0,0 +1,23 @@
+﻿using Rzut.Core;
+using System;
+
+namespace Rzut.WinDesktop
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new RzutGame(Platform.WindowsDesktop))
+                game.Run();
+        }
+    }
+#endif
+}
